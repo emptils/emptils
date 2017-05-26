a1a684f5982dd4c517ce4e81d914aed45a289ed8
liusha <emptils@163.com>
2017-05-26 18:52:20 +0800

    ddd

--- src/main/java/com/emptils/controller/mainController.java
+++ src/main/java/com/emptils/controller/mainController.java
@@ -1,16 +1,17 @@
 package com.emptils.controller;
 
-import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping
 
 public class mainController {
 
-    public void test() {
-        Gson gson = new Gson();
-        gson.
+    @ResponseBody
+    @RequestMapping(value = "/test")
+    public String test() {
+        return "dddddddddddddddd";
     }
 }
