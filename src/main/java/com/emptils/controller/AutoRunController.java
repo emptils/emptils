344a43a215f08b98ca5f8fa6ac2f1f3cd46b4e00
liusha <emptils@163.com>
2017-06-01 18:45:48 +0800

    ddd

---
+++ src/main/java/com/emptils/controller/AutoRunController.java
@@ -0,0 +1,14 @@
+package com.emptils.controller;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Created by Administrator on 2017/6/1.
+ */
+public class AutoRunController implements InitializingBean{
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
---
+++ src/main/java/com/emptils/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.emptils.controller;
+
+
+public class MainController {
+
+    public String test() {
+        return "dddddddddddddddd";
+    }
+}
